1c8d12c57488dfd49866ddc20e644abe1a89b207
Michal Barak <michal.barak01@post.runi.ac.il>
2023-12-22 17:11:33 +0200

    added code for Q3

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,19 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	public static void main(String[] args) {
+		// Created the 3 input values provided by the user
+		// Note that the 3 inputs accept fractions from user (AKA double)
+		double a = Double.parseDouble(args[0]); 
+		double b = Double.parseDouble(args[1]);
+		double c = Double.parseDouble(args[2]);
+		// Calculates the value of x given the 3 input values
+		// Note that in task description we can assume the value of a cannot be 0! 
+		double x = (c - b) / a;
+		// Prints out the linear equation for user to view, then tha calculated value of x
+		System.out.println(a + "*x + " + b + " = " + c);
+		System.out.println("x = " + x);
+
+	}
+
 }
