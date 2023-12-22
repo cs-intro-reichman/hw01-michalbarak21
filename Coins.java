fed819af8d9bbdb711177295c32e044a6685c7a2
Michal Barak <michal.barak01@post.runi.ac.il>
2023-12-22 17:20:53 +0200

    fixed mistake calculations and print in Q2

--- Coins.java
+++ Coins.java
@@ -6,11 +6,11 @@ public class Coins {
 	public static void main(String[] args) {
 		// Created a new input value for the user to submit. 
 		// Note that type of data is integer, as we do not expect to encounter fractions!	
-		int quarters = Integer.parseInt(args[0]);
+		int cents = Integer.parseInt(args[0]);
 		// Calculate the maximal amount of cents that can be exchanged, and the appropriate remainder
-		int cent = (int) (quarters / 25); 
-		int remainder = (int) (quarters - (cent * 25)); 
+		int quarters = (int) (cents / 25); 
+		int remainder = (int) (cents - (quarters * 25)); 
 		// Print the exchange output for the user
-		System.out.println("Use " + remainder + " quarters and " + cent + " cents");
+		System.out.println("Use " + quarters + " quarters and " + remainder + " cents");
 	}
 }
