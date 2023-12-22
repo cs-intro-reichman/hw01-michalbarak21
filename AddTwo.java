991c67ac8496fa0135cca197360a914b9bad370a
Michal Barak <michal.barak01@post.runi.ac.il>
2023-12-22 16:24:21 +0200

    added comments to my code

--- AddTwo.java
+++ AddTwo.java
@@ -3,8 +3,10 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
+        // Created two integers that recieve a value as input from user
+		int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
+		// Print out the sum of both integers (i.e a + b) in the following format: "a + b = sum"
         System.out.println(a + " + " + b + " = " + (a + b));
 	}
 }
